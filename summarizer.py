063f1308c440bde06c96eaaa67c70bbb2c913dc4
Leon Shainski <leon.shainski@gmail.com>
2022-02-01 21:43:52 -0500

    Cleaning up the code, as well as adding a save-to-file feature

--- summarizer.py
+++ summarizer.py
@@ -1,20 +1,57 @@
 #This is an app that will query the SMMRY API to summarize news articles
+from fileinput import filename
 import requests
 import json
 my_headers = {'Authorization' : 'Bearer {access_token}'}
+keyFile = open("key.txt", "r")
+key=keyFile.read()
 
-try:
-    response = requests.get("https://api.smmry.com", params={"SM_API_KEY":"38CCF65DE1", "SM_URL": "https://www.buzzfeednews.com/article/kadiagoba/republican-medical-boards-covid-treatments",})
-    # Code here will only run if the request is successful
-except requests.ConnectionError as error:
-    print(error)
 
-print (response.json())
+#Open the list and split the entries by "," to make an array of URLs
+def readURLS(fileName):
+    f = open("urlList.txt", "r")
+    urls=f.read().split(',')
+    return urls
 
-jsonResponse=response.json()
+#Reading the URLS from a given file
+urls=readURLS("urlList.txt")
 
-summarizedText=jsonResponse["sm_api_content"]
+#Defining an array that will store the URLS for further processing
+urlArr=[]
+for i in range (len(urls)):
+    urlArr.append(urls[i])
 
-f = open("demo.txt", "a")
-f.write(summarizedText)
-f.close()
+#Looping through each URL
+for i in range (len(urlArr)):
+    currentURL=urlArr[i]
+    #Resetting the parameters each loop to reflect the current URL
+    params={
+        "SM_API_KEY":key, 
+        "SM_URL": currentURL
+    }
+    try:
+        #Printing the URL and its type to make sure the program is running OK
+        print(currentURL, type(currentURL))
+
+        # Code here will only run if the request is successful
+        response = requests.get("https://api.smmry.com", params=params)
+        #jsonResponse=response.json()
+        print (response.json())
+
+        #Taking the actual article from the JSON response
+        jsonResponse=response.json()
+        summarizedText=jsonResponse["sm_api_content"]
+
+        #Saving the summarized article to file
+        endFile = open("demoTesting2.txt", "a")
+        endFile.write(summarizedText)
+        endFile.close()
+        
+
+    except requests.ConnectionError as error:
+        print(error)
+
+    
+print("All done!")
+
+
